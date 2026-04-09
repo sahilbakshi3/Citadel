93867ed73813b3ea00355e1f1651b1cfebada5d7
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-04-09 15:55:21 +0530

    feat: integrate tiptap editor with custom toolbar and navbar components

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,10 +3,53 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
+import Table from "@tiptap/extension-table";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import ImageResize from "tiptap-extension-resize-image";
+import Image from "@tiptap/extension-image";
+import { useEditorStore } from "@/store/use-editor-store";
+import Underline from "@tiptap/extension-underline";
+
 import StarterKit from "@tiptap/starter-kit";
 
 export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+
+    onDestroy() {
+      setEditor(null);
+    },
+
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px",
@@ -16,12 +59,34 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      Image,
+      ImageResize,
+      Underline,
       TaskItem.configure({
         nested: true,
       }),
       TaskList,
     ],
-    content: "<p>Hello World! 🌎️</p>",
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     // Don't render immediately on the server to avoid SSR issues
     // immediatelyRender: false,
   });
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdProps {
   params: Promise<{ documentId: string }>;
@@ -9,6 +10,7 @@ const DocumentIdPage = async ({ params }: DocumentIdProps) => {
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
